ee02ec2447c4be477d10557614e5444aa2127ee4
Homero Meireles Junior <65316962+homeromeirelesjunior@users.noreply.github.com>
2021-10-02 12:58:15 -0300

    (02/10) 'conexao_bd'

--- back/api/controllers/autoresControllers.js
+++ back/api/controllers/autoresControllers.js
@@ -3,6 +3,8 @@ const autoresModels = require('../models/autoresModels.js')
 module.exports = {
     autoresMenu,
     autoresGetAll,
+    autoresGetById,
+    autoresAtivoInativo
 }
 
 function autoresMenu(req, res) {
@@ -20,4 +22,50 @@ function autoresGetAll(req, res) {
             res.json(resposta)
         }
     })
+}
+
+function autoresGetById(req, res) {
+    console.log('Consultar Autores')
+    const id = req.params.codigo
+    console.log(`Parâmetro esperado: ${id}`)
+
+    autoresModels.getByIdAutores(id, function(err, resposta) {
+        console.log(`Retorno dos autores`, resposta)
+        if(err) {
+            throw err
+        } else {
+            res.json(resposta)
+        }
+    })
+}
+
+function autoresAtivoInativo(req, res) {
+    console.log('Ativar Inativar Autores')
+    const id = req.params.codigo
+    console.log(`Parâmetro esperado: ${id}`)
+
+    let p_ativo = ""
+
+    autoresModels.getByIdAutores(id, function(err, resposta) {
+        console.log(`Retorno de Autores`, resposta[0].aut_ativoinativo)
+        p_ativo = resposta[0].aut_ativoinativo
+        if (err) {
+            throw err
+        } else {
+            if (p_ativo == "A") {
+                p_ativo = "I"
+            } else {
+                p_ativo = "A"
+            }
+            res.json(resposta)
+        }
+        console.log(`A/I ${p_ativo}`)
+        autoresModels.ativarInativar(id, p_ativo, function(err, result) {
+            if(err) {
+                throw err
+            }
+            console.log('Registro atualizado')
+        })
+    })
+
 }
--- back/api/models/autoresModels.js
+++ back/api/models/autoresModels.js
@@ -1,9 +1,25 @@
 const conexao = require('../../config/conexao.js')
 
 module.exports = {
-    getAllAutores
+    getAllAutores,
+    getByIdAutores,
+    ativarInativar
 }
 
 function getAllAutores (callback) {
     conexao.query('select * from autores', callback)
+}
+
+function getByIdAutores (id, callback) {
+    conexao.query('select * from autores where aut_codigo =' + id, callback)
+}
+
+function ativarInativar (id, ativo, callback) {
+    console.log(`Autores ativando/inativando registro ${id} - Status: ${ativo}`)
+    
+    const m_sql = `update autores set aut_ativoinativo = '${ativo}' where aut_codigo = '${id}'`
+
+    conexao.query(m_sql, callback)
+
+    console.log(`Retornado { M O D E L } Autores Ativando/Inativando registro ${id} - Status ${ativo}`)
 }
